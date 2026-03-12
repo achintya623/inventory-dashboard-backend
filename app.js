00ebd97d34d4a0061a61ec41af97eb6efe29b048
achintya <achintya623@gmail.com>
2026-03-13 01:25:06 +0530

    fix render port

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.use(cors());
 
 app.use("/categories", categoriesRouter);
 app.use("/", itemsRouter);
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`listening to app on port ${PORT}`);
